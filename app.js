622bb4f9023941a0e7104d2ad62313afd5f06e2a
Junho Kim <junho@nntuple.com>
2024-04-16 16:15:35 +0900

    add test route

--- app.js
+++ app.js
@@ -3,9 +3,15 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
+  console.log("/ called")
   res.send('Hello World!')
 })
 
+app.get('/scrapings', (req, res) => {
+    console.log("/scrapings called")
+    res.send('scraping')
+  })
+
 app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`)
 })
